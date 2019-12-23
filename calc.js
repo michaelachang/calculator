619f241307b3e0f5cfa8ec80bb09251b7967f2e1
Michaela Chang <michaela.w.chang@gmail.com>
2019-12-23 09:04:05 -0800

    Added display populate function when buttons are pressed

--- calc.js
+++ calc.js
@@ -1,2 +1,23 @@
-const display = document.querySelector('#display');
+const display = document.querySelector('#display p');
+var cDisplay = "";
 
+const controls = document.querySelectorAll('button');
+console.log(controls);
+
+controls.forEach((node) => {
+    node.addEventListener('click', function(e) {
+        if (node.id.startsWith("num")) {
+            updateDisplay(node.id, 3);
+        } else if (node.id.startsWith("op")) {
+            updateDisplay(node.id, 2);
+        } else if (node.id.startsWith("clear")) {
+            updateDisplay(node.id, 0);
+        }
+    });
+    console.log(node.id);
+})
+
+function updateDisplay(id, start) {
+    cDisplay = start > 0 ? (cDisplay + id.slice(start)) : "";
+    display.textContent = cDisplay;
+}
