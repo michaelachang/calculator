666113ddefd0534291adcfde58b068b972fe84a2
Michaela Chang <michaela.w.chang@gmail.com>
2019-12-23 21:28:31 -0800

    Added function to clear display before populating it, if expression was previously evaluated

--- calc.js
+++ calc.js
@@ -6,12 +6,17 @@ var arg1 = "";
 var arg2 = "";
 var solution = "";
 var op = "";
+var wasEvaluated = false;
 
 controls.forEach(node => {
     node.addEventListener('click', function(e) {
         if (node.id.startsWith("no")) {
             if (!op) {
-                // If expression was previously executed, clear the display and then populate
+                if (wasEvaluated) {
+                    wasEvaluated = false;
+                    arg1 = "";
+                    displayCurrent = "";
+                }
                 arg1 += node.textContent
             } else {
                 arg2 += node.textContent
@@ -79,6 +84,7 @@ function operate() {
 
     arg2 = op = solution = "";
     arg1 = displayCurrent;
+    wasEvaluated = true;
 
 
     // Converts numbers and operation symbols to expression and evaluates expression
@@ -95,77 +101,3 @@ function updateDisplay(node) {
     displayCurrent += node.textContent;
     display.textContent = displayCurrent;
 }
-
-/* var num1 = "";
-var num2 = "";
-var numSolution = "";
-var op = "";
-
-const controls = document.querySelectorAll('button');
-console.log(controls);
-
-controls.forEach((node) => {
-    node.addEventListener('click', function (e) {
-        if (node.id.startsWith("num")) {
-            if (op) {
-                num2 += node.textContent;
-            } else {
-                num1 += node.textContent;
-            }
-            updateDisplay(node, 3);
-        } else if (node.id.startsWith("op")) {
-            // Checks if operation has already been selected once
-            if (op) {
-                operate(op);
-                op = node.id.slice(2);
-            } else {
-                op = node.id.slice(2);
-            }
-            updateDisplay(node, 2);
-        } else if (node.id.startsWith("clear")) {
-            updateDisplay(node, 0);
-        } else if (node.id.startsWith("eq")) {
-            display.textContent = operate(op);
-        }
-    });
-    console.log(node.id);
-})
-
-function updateDisplay(node, start) {
-    cDisplay = start > 0 ? (cDisplay + node.textContent) : "";
-    display.textContent = cDisplay;
-}
-
-function operate(op) {
-    if (!num2 && num1) {
-        return num1;
-    }
-
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    
-    switch (op) {
-        case "/": 
-            numSolution = num1 / num2;
-            break;
-
-        case "*": 
-            numSolution = num1 * num2;
-            break;
-
-        case "-": 
-            numSolution = num1 - num2;
-            break;
-
-        case "+": 
-            numSolution = num1 + num2;
-            break;
-    }
-
-    console.log(numSolution);
-    cDisplay = numSolution;
-    num1 = numSolution;
-    num2 = "";
-    op = "";
-    return numSolution;
-} */
