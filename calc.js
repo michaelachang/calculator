9911abdf790de34443f2dd688848390d3f8ca417
Michaela Chang <michaela.w.chang@gmail.com>
2019-12-23 21:18:47 -0800

    Refactored calculator code

--- calc.js
+++ calc.js
@@ -1,7 +1,102 @@
 const display = document.querySelector('#display p');
-var cDisplay = "";
+var displayCurrent = "";
 
-var num1 = "";
+const controls = document.querySelectorAll('button');
+var arg1 = "";
+var arg2 = "";
+var solution = "";
+var op = "";
+
+controls.forEach(node => {
+    node.addEventListener('click', function(e) {
+        if (node.id.startsWith("no")) {
+            if (!op) {
+                // If expression was previously executed, clear the display and then populate
+                arg1 += node.textContent
+            } else {
+                arg2 += node.textContent
+            }
+
+            updateDisplay(node);
+
+            // Populate display with number
+            // If expression was previously executed, clear the display and then populate
+        }
+
+        if (node.id.startsWith("op")) {
+            updateDisplay(node);
+            (!op) ? (op += node.id.slice(2)) : (operate());
+            
+            // Store operator in variable
+            // If expression was previously executed, apply operation to resulting value
+            // If there is already an operator stored, call the operate function and apply operation to resulting value.
+        }
+
+        if (node.id.startsWith("clear")) {
+            updateDisplay();
+            // Clear all variables and display
+        }
+
+        if (node.id.startsWith("eq")) {
+            operate();
+            // Call the operate function
+            // If pressed after an operation, when the display is empty, or when dividing by zero, throw an error
+        }
+    })
+})
+
+function operate() {
+    if (!arg2 && arg1) {
+        solution = arg1;
+        displayCurrent = solution.toString();
+        display.textContent = displayCurrent;
+    }
+
+    arg1 = parseFloat(arg1);
+    arg2 = parseFloat(arg2);
+
+    switch (op) {
+        case "/":
+            if (!(arg2 === 0)) {
+                solution = arg1 / arg2;
+            } else {
+                solution = "ERR CANNOT DIVIDE BY 0";
+            }
+            break;
+        case "*":
+            solution = arg1 * arg2;
+            break;
+        case "-":
+            solution = arg1 - arg2;
+            break;
+        case "+":
+            solution = arg1 + arg2;
+            break;
+    }
+
+    displayCurrent = solution.toString();
+    display.textContent = displayCurrent;
+
+    arg2 = op = solution = "";
+    arg1 = displayCurrent;
+
+
+    // Converts numbers and operation symbols to expression and evaluates expression
+    // Rounds decimal numbers to 9 places
+}
+
+function updateDisplay(node) {
+    if(!node) {
+        displayCurrent = "";
+        arg1 = arg2 = op = solution = "";
+        display.textContent = displayCurrent; 
+    }
+
+    displayCurrent += node.textContent;
+    display.textContent = displayCurrent;
+}
+
+/* var num1 = "";
 var num2 = "";
 var numSolution = "";
 var op = "";
@@ -50,10 +145,21 @@ function operate(op) {
     num2 = parseFloat(num2);
     
     switch (op) {
-        case "/": numSolution = num1 / num2;
-        case "*": numSolution = num1 * num2;
-        case "-": numSolution = num1 - num2;
-        case "+": numSolution = num1 + num2;
+        case "/": 
+            numSolution = num1 / num2;
+            break;
+
+        case "*": 
+            numSolution = num1 * num2;
+            break;
+
+        case "-": 
+            numSolution = num1 - num2;
+            break;
+
+        case "+": 
+            numSolution = num1 + num2;
+            break;
     }
 
     console.log(numSolution);
@@ -62,4 +168,4 @@ function operate(op) {
     num2 = "";
     op = "";
     return numSolution;
-}
+} */
